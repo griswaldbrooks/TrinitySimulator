1723d143ff6a48ff515d49507bf60b6e15b2f097
Griswald Brooks <ricecracker02@gmail.com>
2013-03-11 02:52:23 -0400

    Started Local Map Navigation Algorithm for Artemis

--- Artemis v1/robotNav_RA1.m
+++ Artemis v1/robotNav_RA1.m
@@ -6,6 +6,7 @@
 %     om        Robot angular velocity in radians/sec
 function [v, om] = robotNav_RA1(ranges)
 
-om = 0.6*atan(ranges(3) - ranges(1));
-v = exp(-(om^2))*log(mean([ranges(1),ranges(3)])/23);
+%om = 0.6*atan(ranges(3) - ranges(1));
+om = 1*(atan(ranges(3)) - atan(ranges(1)));
+v = 1*exp(-(om^2))*log(mean([ranges(1),ranges(3)])/30)*log(ranges(2)/50);
 
